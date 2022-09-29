66b6fcddfdf282e716986e5f5fd0a72e73ebbadf
im3dabasia <eshaandabasiya@gmail.com>
2022-09-29 11:18:41 +0530

    Made the app mobile resposive and added keyboard events

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 let evaluationString = "";
 const answerBox = document.getElementById('answer-display')
-const myFunction = (arg) => {
 
-    if (arg === 'EQ') {
+
+window.addEventListener("keydown", (event) => {
+
+    if(Number(event.key) || event.key ==='*' || event.key ==='+' || event.key ==='-' || event.key ==='/'){
+        evaluationString += event.key
+        answerBox.innerHTML  = evaluationString
+
+    }
+    if(event.key ==='Enter'){
+        let ans = eval(evaluationString)
+
+        answerBox.innerHTML  = ans ? ans : "0" ;
+        evaluationString =""
+
+    }
+    if(event.key ==='Backspace'){
+        evaluationString =  evaluationString.slice(0,evaluationString.length -1)
+
+        console.log(evaluationString)
+        answerBox.innerHTML  = evaluationString
+
+    }
+}, true);
+
+const myFunction = (arg) => {
+    
+
+    if((arg === '*' && evaluationString.charAt(0) === '*') || (arg === '/' && evaluationString.charAt(0) === '/')){
+
+        evaluationString = "";
+        answerBox.innerHTML = evaluationString
+
+        console.log(arg.length )
+    }
+
+    else if (arg === 'EQ') {
 
         let ans = eval(evaluationString)
 
@@ -15,7 +49,6 @@ const myFunction = (arg) => {
 
         }
     }
-
     else if (arg === 'AC') {
         evaluationString = ""
         answerBox.innerHTML = evaluationString
